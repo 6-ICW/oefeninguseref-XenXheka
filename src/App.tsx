49c4738ddba8f614b67340ba8a95c6c34efa35f7
Xen <Xen.Xheka@student.tsaam.be>
2026-03-19 12:08:43 +0100

    Versie2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
+import UserForm from "./components/UserForm";
 
 function App() {
-
-  useState([{ id: 1, omschrijving: 'leerling' }, { id: 2, omschrijving: 'leerkracht' }, { id: 3, omschrijving: 'administratief medewerker' },])
-
+  const [typeUser, setTypeUser] = useState([
+    { id: 1, omschrijving: "leerling" },
+    { id: 2, omschrijving: "leerkracht" },
+    { id: 3, omschrijving: "administratief medewerker" },
+  ]);
+  const [users, setUsers] = useState<string[]>([]);
+  const handleClick = (user?: string) => {
+    if (user != null) setUsers([...users, user]);
+  };
   return (
     <>
-
+      <UserForm onSubmit={handleClick}></UserForm>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,14 +1,28 @@
-export default function UserForm() {
+import { useRef } from "react";
+
+interface Props {
+  onSubmit: (value?: string) => void;
+}
+export default function UserForm({ onSubmit }: Props) {
+  const firtstName = useRef<HTMLInputElement>(null);
+  const LaststName = useRef<HTMLInputElement>(null);
+  const TypeUser = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <form>
-        <input type="text" id="firstName" />
-        <input type="text" id="lastName" />
-        <input type="text" id="age" />
+        <input ref={firtstName} type="text" id="firstName" />
+        <input ref={LaststName} type="text" id="lastName" />
+        <input ref={TypeUser} type="text" id="age" />
         <select name="typeUser" id="typeUser">
           {/* type medewerker */}
         </select>
-        <button type="submit">Registreer</button>
+        <button
+          onClick={() => onSubmit(firtstName.current?.value)}
+          type="submit"
+        >
+          Registreer
+        </button>
       </form>
     </>
   );
